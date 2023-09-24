f562d13cec9bf55592ea43ce2f3778aa1ec5591b
ShaikhSohel <sh1052887120@gmail.com>
2023-09-24 12:13:31 +0530

    Notification Feature

--- src/Utility_Component/OnDeleteModel.jsx
+++ src/Utility_Component/OnDeleteModel.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
